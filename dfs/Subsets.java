b2c9b612040017f79f893795d13d0ef1befc717a
JunjieZhong <junjiezhong720@gmail.com>
2017-05-04 18:06:49 -0400

    update

---
+++ dfs/Subsets.java
@@ -0,0 +1,38 @@
+// 递归：一种实现DFS算法的一种方式
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+	
+	ArrayList<List<Integer>> results = new ArrayList<List<Integer>>();
+
+	if(nums == null){
+	  return results;
+	}
+
+	if(nums.length == 0){
+	  results.add(new ArrayList<Integer>());
+	  return results;
+	}
+
+	subsetsHelper(new ArrayList<Integer>(), nums, 0, results);
+	return results;      
+    }
+// 递归三要素
+// 1. 递归的定义：在 nums 中找到所有以 subset 开头的的集合，并放到 results 
+   private void subsetsHelper(ArrayList<Integer> subset, int[] nums, int startIndex, ArrayList<List<Integer>> results){
+
+	
+	results.add(new ArrayList<Integer>(subset));
+
+	for(int i = startIndex; i < nums.length; i++){
+          // 找以[1]开头的所有集合
+          // [1] -> [1,2]
+	  subset.add(nums[i]);
+          // 寻找所有以 [1,2] 开头的集合，并扔到 results
+	  subsetsHelper(subset, nums, i + 1, results);
+	  // [1,2] -> [1]  回溯
+	  subset.remove(subset.size() - 1);
+	}
+        // 3. 递归的出口
+        // return;	
+    }
+}
---
+++ dfs/SubsetsII.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    ArrayList<List<Integer>> results = new ArrayList<List<Integer>>();
+
+	if(nums == null){
+	  return results;
+	}
+
+	if(nums.length == 0){
+	  results.add(new ArrayList<Integer>());
+	  return results;
+  }
+	//先要排序
+        Arrays.sort(nums);
+	subsetsHelper(new ArrayList<Integer>(), nums, 0, results);
+	return results;      
+    }
+    
+   private void subsetsHelper(ArrayList<Integer> subset, int[] nums, int startIndex, ArrayList<List<Integer>> results){
+	
+	results.add(new ArrayList<Integer>(subset));
+
+	for(int i = startIndex; i < nums.length; i++){
+	  //只关心取多少个2，不关心取哪几个，从第一个2开始连续取
+	  if(i != startIndex && nums[i] == nums[i - 1])
+	    continue;
+	  subset.add(nums[i]);
+	  subsetsHelper(subset, nums, i + 1, results);
+	  subset.remove(subset.size() - 1);
+	}
+  }
+}

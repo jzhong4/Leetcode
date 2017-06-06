626610b12f0a9c6de529d6d162e07998b97acc08
JunjieZhong <junjiezhong720@gmail.com>
2017-06-06 16:10:09 -0400

    update

---
+++ LinkedList/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,44 @@
+/*
+ Author:     Junjie
+ Date:       June 6, 2017
+ Problem:    Remove Duplicates from Sorted List
+ Difficulty: Easy
+ Source:     http://www.lintcode.com/en/problem/remove-duplicates-from-sorted-list
+ Given a sorted linked list, delete all duplicates such that each element appear only once.
+ Example
+ Given 1->1->2, return 1->2.
+ Given 1->1->2->3->3, return 1->2->3.
+ Solution: 一根指针扫描，删除重复，否则查看下一个数。
+*/
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param ListNode head is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public static ListNode deleteDuplicates(ListNode head) { 
+        if (head == null){
+            return head;
+        }
+        
+        ListNode node = head;
+        while(node.next != null){
+            if (node.val == node.next.val){
+                node.next = node.next.next;
+            }else{
+                node = node.next;
+            }
+        }
+        return head;
+    }  
+}
---
+++ LinkedList/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,49 @@
+/*
+ Author:     Junjie
+ Date:       June 6, 2017
+ Problem:    Remove Duplicates from Sorted List II
+ Difficulty: Medium
+ Source:     http://www.lintcode.com/en/problem/remove-duplicates-from-sorted-list-ii
+ Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+ Example
+ Given 1->2->3->3->4->4->5, return 1->2->5.
+ Given 1->1->1->2->3, return 2->3.
+*/
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param ListNode head is the head of the linked list
+     * @return: ListNode head of the linked list
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while(head.next != null && head.next.next != null){
+            if(head.next.val == head.next.next.val){
+              int val = head.next.val;
+              while(head.next != null && head.next.val == val){
+                head.next = head.next.next;
+                }   
+            }else{
+                head = head.next; 
+            }
+        }
+        return dummy.next;
+    }
+}

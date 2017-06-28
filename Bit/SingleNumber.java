921a673d5a4526e9ed7f68d15cee2715e3819835
JunjieZhong <junjiezhong720@gmail.com>
2017-06-27 23:19:45 -0400

    update

---
+++ Bit/SingleNumber.java
@@ -0,0 +1,29 @@
+/*
+ Author:     Junjie
+ Date:       June 27, 2017
+ Problem:    Single Number
+ Difficulty: Easy
+ Source:     http://www.lintcode.com/en/problem/single-number
+ Given 2*n + 1 numbers, every numbers occurs twice except one, find it.
+ Example
+ Given [1,2,2,1,3,4,3], return 4
+ Solution: 异或： 1.不进位加法 2.相同为0，不同为1
+                 3.a ^ a = 0 4.a ^ b ^ c =  (a ^ b) ^ c = a ^ (b ^ c)
+*/
+public class Solution {
+    /**
+      *@param A : an integer array
+      *return : a integer 
+      */
+    public int singleNumber(int[] A) {
+        if(A == null || A.length == 0){
+            return 0;
+        }
+        
+        int result = 0;
+        for(int i = 0; i < A.length; i++){
+            result ^= A[i];
+        }
+        return result;
+    }
+}
---
+++ Bit/SingleNumberIII.java
@@ -0,0 +1,38 @@
+/*
+ Author:     Junjie
+ Date:       June 27, 2017
+ Problem:    Single Number III
+ Difficulty: Medium
+ Source:     http://www.lintcode.com/en/problem/single-number-iii/
+ Given 2*n + 2 numbers, every numbers occurs twice except two, find them.
+ Example
+ Given [1,2,2,3,4,4,5,3] return 1 and 5
+ Solution: XOR, 分成最后一位上是0和1两组
+*/
+public class Solution {
+    /**
+     * @param A : An integer array
+     * @return : Two integers
+     */
+    public List<Integer> singleNumberIII(int[] A) {
+        int xor = 0;
+        for(int i = 0; i < A.length; i++){
+            xor ^= A[i];
+        }
+        
+        int lastBit = xor - (xor & (xor - 1));
+        int group0 = 0, group1 = 0;
+        for(int i = 0; i < A.length; i++){
+            if((lastBit & A[i]) == 0){
+                group0 ^= A[i];
+            }else{
+                group1 ^= A[i];
+            }
+        }
+        
+        List<Integer> result = new ArrayList<Integer>();
+        result.add(group0);
+        result.add(group1);
+        return result;
+    }
+}
